e1ed962a102e314c8cf5cadf5b35281c3d82cad0
VictoriaCarvalho <vix219@icloud.com>
2025-03-09 15:50:40 -0500

    add lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+def add(num1=45, num2=55):
+    return  num1 + num2
 
-def halve(number):
-    pass
+def halve(number= 100):
+    return number / 2
